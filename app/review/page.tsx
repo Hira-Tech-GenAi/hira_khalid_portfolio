6570a067d3d2577d21015487ed31b811b2b5b78e
Hira Khalid <hiratech88@gmail.com>
2024-11-06 11:32:50 +0500

    portfolio nextapp

---
+++ app/review/page.tsx
@@ -0,0 +1,12 @@
+import Reviews from "@/components/Home/reviews/Reviews";
+import React from "react";
+
+const Review = () => {
+  return (
+    <div>
+      <Reviews />
+    </div>
+  );
+};
+
+export default Review;
---
+++ constant/constant.ts
@@ -0,0 +1,29 @@
+import { url } from "inspector";
+
+export const navLinks = [
+  {
+    id: 1,
+    url: "/",
+    label: "Home",
+  },
+  {
+    id: 2,
+    url: "/aboutus",
+    label: "About",
+  },
+  {
+    id: 3,
+    url: "/myservices",
+    label: "Services",
+  },
+  {
+    id: 5,
+    url: "/review",
+    label: "Reviews",
+  },
+  {
+    id: 6,
+    url: "/contactus",
+    label: "Contacts",
+  },
+];
